6be3cde91cd8a85960cf8b00d6041880607cb3b4
Jose Padilla <jpadilla1@users.noreply.github.com>
2015-01-02 23:05:20 -0400

    fixes terrifying migrations mistake

--- showcase_backend/departments/migrations/0001_initial.py
+++ showcase_backend/departments/migrations/0001_initial.py
@@ -7,7 +7,6 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('universities', '0001_initial'),
     ]
 
     operations = [
@@ -15,31 +14,10 @@ class Migration(migrations.Migration):
             name='Department',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='DepartmentName',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=b'100')),
             ],
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='department',
-            name='name',
-            field=models.ForeignKey(to='departments.DepartmentName'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='department',
-            name='university',
-            field=models.ForeignKey(to='universities.University'),
-            preserve_default=True,
         ),
     ]
--- showcase_backend/users/migrations/0001_initial.py
+++ showcase_backend/users/migrations/0001_initial.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.utils.timezone
+import showcase_backend.users.mixins
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('departments', '0002_remove_department_university'),
-        ('universities', '0001_initial'),
+        ('departments', '__first__'),
+        ('universities', '__first__'),
     ]
 
     operations = [
@@ -21,18 +22,17 @@ class Migration(migrations.Migration):
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
                 ('first_name', models.CharField(max_length=40)),
                 ('last_name', models.CharField(max_length=40)),
-                ('email', models.EmailField(max_length=40)),
+                ('email', models.EmailField(unique=True, max_length=40)),
                 ('gravatar_url', models.URLField(blank=True)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('is_superuser', models.BooleanField(default=False)),
                 ('is_active', models.BooleanField(default=True)),
-                ('token_version', models.CharField(default=b'3353d8ef-5b58-4ce7-aa6a-1f276d335500', unique=True, max_length=36, db_index=True)),
                 ('department', models.ForeignKey(to='departments.Department')),
                 ('university', models.ForeignKey(to='universities.University')),
             ],
             options={
                 'abstract': False,
             },
-            bases=(models.Model,),
+            bases=(models.Model, showcase_backend.users.mixins.ModelDiffMixin),
         ),
     ]
