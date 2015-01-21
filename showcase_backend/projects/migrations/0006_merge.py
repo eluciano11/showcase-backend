e27c847e1a459871e265d1c07ad629de3f52897e
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-01-21 19:31:20 -0400

    merged migrations

---
+++ showcase_backend/projects/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('projects', '0005_auto_20150120_0347'),
+        ('projects', '0005_auto_20150121_0142'),
+    ]
+
+    operations = [
+    ]
